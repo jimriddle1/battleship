e4a75c8c6277d5d33df22d7be6c423a8be93b42c
Jim Riddle <jimriddle0@gmail.com>
2023-01-02 11:49:07 -0600

    wrote test for rendering a cell, tests passing

--- lib/cell.py
+++ lib/cell.py
@@ -4,12 +4,19 @@ class Cell:
     self.is_empty = True
     self.ship = None
     self.fired_upon = False
+    self.render = "."
 
   def place_ship(self, input_ship):
     self.ship = input_ship
     self.is_empty = False
+    self.render = "S"
 
   def fire_upon(self):
+    if self.is_empty == True:
+       self.render = "M"
     if self.is_empty == False:
        self.ship.hit()
        self.fired_upon = True
+       self.render = "H"
+       if self.ship.health <= 0:
+          self.render = "X"
